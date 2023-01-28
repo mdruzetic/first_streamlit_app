bafac6521164d50ed92cb09367e55c915d44d2f8
mdruzetic <63796073+mdruzetic@users.noreply.github.com>
2023-01-28 17:58:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-import pandas
 
 streamlit.title('This is the first streamlit code')
 streamlit.header('Jelovnik')
@@ -7,6 +6,7 @@ streamlit.text('🥣 Juha')
 streamlit.text('🐔 🍞 Glavno jelo')
 streamlit.text('🥗 Prilog')
 streamlit.text('🥑 Desert')
-   
+
+import pandas
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
