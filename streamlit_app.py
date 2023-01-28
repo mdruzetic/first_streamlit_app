cad177f4304902479a968080bc55b4814dd1a0e5
mdruzetic <63796073+mdruzetic@users.noreply.github.com>
2023-01-28 17:45:40 +0100

    Update streamlit_app.py
    dodao cijeli menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('This is the first streamlit code')
+streamlit.header('Jelovnik')
+streamlit.text('Juha')
+streamlit.text('Glavno jelo')
+streamlit.text('Prilog')
+streamlit.text('Desert')
