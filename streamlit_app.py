d2ac798fb6ddccb94f0d9dbbd5848b88a3a039d0
mdruzetic <63796073+mdruzetic@users.noreply.github.com>
2023-01-28 17:48:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('This is the first streamlit code')
 streamlit.header('Jelovnik')
-streamlit.text('Juha')
-streamlit.text('Glavno jelo')
-streamlit.text('Prilog')
-streamlit.text('Desert')
+streamlit.text('🥣 Juha')
+streamlit.text('🐔 🍞 Glavno jelo')
+streamlit.text('🥗 Prilog')
+streamlit.text('🥑 Desert')
+   
