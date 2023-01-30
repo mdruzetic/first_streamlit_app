4a0e12dadd435ab43a096944dd5c579360eb16dd
mdruzetic <63796073+mdruzetic@users.noreply.github.com>
2023-01-30 09:27:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,23 @@
+import streamlit
+
+streamlit.title('This is the first streamlit code')
+streamlit.header('Jelovnik')
+streamlit.text('🥣 Juha')
+streamlit.text('🐔 🍞 Glavno jelo')
+streamlit.text('🥗 Prilog')
+streamlit.text('🥑 Desert')
+
+# import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Grapes'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
-
-
-# import streamlit
-
-# streamlit.title('This is the first streamlit code')
-# streamlit.header('Jelovnik')
-# streamlit.text('🥣 Juha')
-# streamlit.text('🐔 🍞 Glavno jelo')
-# streamlit.text('🥗 Prilog')
-# streamlit.text('🥑 Desert')
-
-# import pandas
-# my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Grapes'])
-# fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
-# streamlit.dataframe(fruits_to_show)
